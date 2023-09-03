09758058bb32f1b8fd3037541eede71c9acb2bb7
AtulPandey429 <pandeyatul429x@gmail.com>
2023-09-03 15:58:40 +0530

    app.js updated

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const morgan = require("morgan");
-const path = require("path");
 const db = require("./Config/db");
 
 const app = express();
@@ -13,22 +11,12 @@ db();
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(morgan("dev"));
 
 // API routes
 app.use("/api/user", require("./routers/CreateUser"));
 app.use("/api/user", require("./routers/Display"));
 app.use("/api/user", require("./routers/Order"));
 
-// Serve static files
-app.use(express.static(path.join(__dirname, "./client/build")));
-
-// Serve React app for any other routes
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
-// Start the server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
